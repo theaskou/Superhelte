59a036342b0efe8dc8e62ade4779a4924ee3d0cb
theaskou <thsk0001@stud.kea.dk>
2023-09-16 21:42:18 +0200

    Userinterface added Search function added

--- src/Database.java
+++ src/Database.java
@@ -11,7 +11,7 @@ public class Database {
         Superhero batwoman = new Superhero("Batwoman", "Kathy Kane", "Martial arts and stealth", 1956, "Human", 9);
         Superhero batman = new Superhero("Batman", "Bruce Wayne", "Wealth, strength, strategist", 1939, "Human", 8);
         Superhero spiderman = new Superhero("Spiderman", "Peter Parker", "Spider-mutant powers", 1962, "Human mutant", 6);
-        Superhero wonderwoman = new Superhero("Wonderwoman", "Diana Prince", "Superhuman strength, flight", 1941, "Demi-godess", 10);
+        Superhero wonderwoman = new Superhero("Wonderwoman", "Diana Prince", "Superhuman strength, flying", 1941, "Demi-godess", 10);
 
         // Tilføj superheltene til listen:
         superheroes.addAll(Arrays.asList(superman, batwoman, batman, spiderman, wonderwoman));
@@ -30,5 +30,39 @@ public class Database {
         }
     }
 
-}
+    // Metode til at søge efter en superhelt i databasen:
+    // Virker ikke på tal
+    // Skal håndtere søgninger der ikke giver resultater
+    // Udskrivning skal gøres bedre, der kommer [ og , med.
+    // Der må findes en simplere måde at kalde alle attributter i funktionen.
+
+    // font colors
+    final String BLACK_BOLD = "\033[1;30m";
+    final String RESET = "\033[0m";
+    public ArrayList<String> søgeFunktion (String søgeOrd) {
+
+        ArrayList<String> søgning = new ArrayList<>();
+
+        for (Superhero superhero : superheroes) {
+            if (superhero.getName().toLowerCase().contains(søgeOrd.toLowerCase()) || superhero.getRealName().toLowerCase().contains(søgeOrd.toLowerCase()) || superhero.getIsHuman().toLowerCase().contains(søgeOrd.toLowerCase()) || superhero.getSuperPowers().toLowerCase().contains(søgeOrd.toLowerCase())) {
+
+                if (!søgning.contains(superhero.getName()) || !søgning.contains(superhero.getRealName()) || !søgning.contains(superhero.getIsHuman()) || !søgning.contains(superhero.getSuperPowers())) {
+                   søgning.add(BLACK_BOLD + "\nNavn: " + RESET + superhero.getName() + BLACK_BOLD + " Rigtigt navn: " + RESET + superhero.getRealName() + BLACK_BOLD + " Superkræfter: " + RESET + superhero.getSuperPowers() + BLACK_BOLD + " Oprindelsesår: " + RESET + superhero.getYearCreated() + BLACK_BOLD + " Race: " + RESET + superhero.getIsHuman() + BLACK_BOLD + " Styrke: " + RESET + superhero.getStrength() + "\n");
+                }
+            }
+        }
+            return søgning;
+
+        }
+
+        // Metode til at redigere i listerne:
+
+
+
+
+
+
+    }
+
+
 
